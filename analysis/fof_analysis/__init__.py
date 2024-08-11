53708bc56e40a871efa49601ab886c308cadd622
maximilian <macozzi2021@gmail.com>
2024-08-10 20:44:08 -0700

    Initial check in

---
+++ analysis/fof_analysis/__init__.py
@@ -0,0 +1,1 @@
+from fof_analysis import fof
---
+++ analysis/utilities/__init__.py
@@ -0,0 +1,2 @@
+from . import array, binning, constant, coordinate, io, math, plot
+from . import catalog, cosmology, halo_property, orbit, particle, simulation
